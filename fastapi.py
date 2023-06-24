885b549b67799cabf55cb0d6b075ca585cd731c8
Rustam Zhientaev <zhientaev322@gmail.com>
2023-06-24 17:24:01 +0500

    first commit

---
+++ fastapi.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bdbf1ef7c41046554e6dadcf1267a5c7cc6f8ac912a1529a1fd27d5162bc6973
+size 188
---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa17bc26bc3751b4a964d5e1b8cadcdb4f1de9abfd02a1a225dc3132dfcc3067
+size 4360
