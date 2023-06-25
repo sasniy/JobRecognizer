aa1839492f6344c1c37ea2be5216effadc957e00
Rustam Zhientaev <zhientaev322@gmail.com>
2023-06-25 06:53:18 +0500

    added method for excel

--- app/site.py
+++ app/site.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9246d17616ceb2c782642c880b7b6118c49c95ec39d715fcaf3a3a1d1bbea0ff
-size 1693
+oid sha256:7f7bcad3d445946775d8a0830052b5ffb47dd9039f84ba658392ec9e7493a528
+size 1955
---
+++ data_proccesing.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f57ee456d994b352cdee91bc08a3ec3293748fe05e26872d153b1f5546ff3c3
+size 1620
