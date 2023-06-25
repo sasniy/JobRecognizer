7747003671d16a0d59360490533f2e3617a24726
Rustam Zhientaev <zhientaev322@gmail.com>
2023-06-25 10:20:17 +0500

    a

--- model.py
+++ model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89886bfec8ecaa5f5ddae5e366d3dc6dcb9768f9c885228d41d2eca70d04cd2b
-size 4518
+oid sha256:7e62d0dc6850addab33cfc2e56ab3b0677c2c856c1981d205b6b52663b613fc7
+size 4365
