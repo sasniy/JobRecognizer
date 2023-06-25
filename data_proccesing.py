81b7f1bf9066cc0fe19103aabb346c5145e9d2c7
Rustam Zhientaev <zhientaev322@gmail.com>
2023-06-25 07:05:08 +0500

    tune model

--- data_proccesing.py
+++ data_proccesing.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f57ee456d994b352cdee91bc08a3ec3293748fe05e26872d153b1f5546ff3c3
-size 1620
+oid sha256:4c4338e0b9c83638473db537c2c1de1491c586068b9435d89bf84fed6bc9212f
+size 1607
